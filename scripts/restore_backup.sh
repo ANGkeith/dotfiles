043006dca5378158213376a28df8e103038ca3ad
Keith Ang <keith.ang@dathena.io>
2019-07-24 09:19:16 +0800

    fix script

--- scripts/restore_backup.sh
+++ scripts/restore_backup.sh
@@ -6,7 +6,7 @@ cp ./.bashrc ~/.bashrc
 # i3
 cp -r ./i3 ~/.config/
 cp -r ./rofi ~/.config/
-sudo cp ./i3-persist.sh /usr/bin/i3-persist
+sudo cp ./scripts/i3-persist.sh /usr/bin/i3-persist
 cp ./.fehbg ~/.fehbg
 cp wallpaper.jpg ~/Pictures/wallpaper.jpg
 cp .compton.conf ~/.compton.conf
