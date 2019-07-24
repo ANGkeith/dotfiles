9375dd1b9bb90fa7de218a45aeb3c900f0c90393
ANGkeith <kang024@e.ntu.edu.sg>
2019-07-24 22:44:15 +0800

    install docker

--- scripts/etc_install.sh
+++ scripts/etc_install.sh
@@ -2,13 +2,20 @@
 sudo apt install -y vim vim-gnome i3 kdiff3 xclip git gitk wget curl make build-essential \
 libssl-dev tmux
 
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
 sudo apt install -y python3-pip
 
 pip3 install --user pipenv
 pip3 install virtualenv
 pip3 install virtualenvwrapper
 
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-
 pip3 install powerline-status
 
+# install docker
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
