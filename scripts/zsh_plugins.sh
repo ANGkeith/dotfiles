b6b88de04f221614cdf38bdc4a396b70c57dac3a
ANGkeith <kang024@e.ntu.edu.sg>
2019-08-20 02:07:09 +0800

    zsh plugins

---
+++ scripts/zsh_plugins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
