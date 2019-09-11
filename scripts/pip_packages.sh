e66a0c4890ce98325c875c94e67b716c17e1f49f
ANGkeith <kang024@e.ntu.edu.sg>
2019-09-11 19:22:02 +0800

    jupyter with vim plugins

--- scripts/pip_packages.sh
+++ scripts/pip_packages.sh
@@ -10,3 +10,12 @@ python -m pip install termcolor
 # ranger dependency
 sudo apt install -y w3m-img
 python -m pip install ranger-fm
+
+# jupyter
+mkvirtualenv jupyter
+pip install jupyterlab
+pip install jupyter-nbextensions-configurator
+mkdir -p $(jupyter --data-dir)/nbextensions/
+git clone https://github.com/lambdalisue/jupyter-vim-binding $(jupyter --data-dir)/nbextensions/vim_binding
+
+
