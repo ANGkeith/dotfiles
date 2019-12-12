4481a246867af7e1c31d50169aa4dcfe5abde636
ANGkeith <kang024@e.ntu.edu.sg>
2019-12-13 00:10:15 +0800

    use stow to manage dotfiles

---
+++ scripts/stow.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+script_root="$(cd $(dirname $BASH_SOURCE[0]) && pwd -P)"
+project_root=$(cd $(dirname ${script_root}) && pwd -P)
+
+cd ${project_root}/stow
+stow -v -S -t ~ *
