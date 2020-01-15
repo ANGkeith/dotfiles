77341cb79661b01c0827d0d256d900c09b108723
ANGkeith <kang024@e.ntu.edu.sg>
2020-01-15 11:03:46 +0800

    Change to a more minimal polybar

--- stow/polybar/.config/polybar/launch.sh
+++ stow/polybar/.config/polybar/launch.sh
@@ -6,8 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload main &
+    done
+else
+    polybar --reload main &
+fi
 # Launch polybar
-polybar main &
 sleep 0.4
 
 # need to run this after launching bar otherwise the format tag cannot be parsed
