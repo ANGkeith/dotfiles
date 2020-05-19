8040a6c77f62b3b7a86d4572fea5cf2468294eaf
ANGkeith <keith_ang@epam.com>
2020-05-19 10:34:08 +0800

    set default indent for js file

--- stow/emacs/.config/doom/+filetype.el
+++ stow/emacs/.config/doom/+filetype.el
@@ -1,5 +1,7 @@
 ;;; ~/dotfiles/stow/emacs/.config/doom/+filetype.el -*- lexical-binding: t; -*-
 
+;; js2
 (map! :map js2-mode-map
       :localleader
       "f" #'my-js-mode-lint)
+(setq js-indent-level 2)
