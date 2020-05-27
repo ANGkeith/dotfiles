28e98d45e151c97cb51429edef3e80db5e977cc5
ANGkeith <angkeith@hotmail.sg>
2020-05-27 23:27:04 +0800

    more settings for html mode

--- stow/emacs/.config/doom/+filetype.el
+++ stow/emacs/.config/doom/+filetype.el
@@ -13,6 +13,15 @@
 (setq-hook! 'typescript-mode-hook
   typescript-indent-level 2)
 
+;; html
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-indent-style 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
 ;; sh-mode
 (setq-hook! 'sh-mode-hook
   flycheck-checker (if (eq sh-shell 'zsh) 'sh-zsh 'sh-shellcheck))
