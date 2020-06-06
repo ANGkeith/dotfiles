5df7b73c957b1ae41059b2218a1499a29c9fa359
ANGkeith <angkeith@hotmail.sg>
2020-06-06 17:25:17 +0800

    fix some typo and append --neeeded flag to pacman
    --needed flag will not reinstall the packages if it is already installed

--- scripts/arch_setup.sh
+++ scripts/arch_setup.sh
@@ -9,138 +9,139 @@ SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 # shellcheck disable=SC2034
 PROJECT_ROOT=$(cd "$(dirname "${SCRIPT_DIRECTORY}")" && pwd -P)
 
-sudo pacman -S xorg-server xorg-xinit xorg-xhost --noconfirm
+sudo pacman -S xorg-server xorg-xinit xorg-xhost --noconfirm --needed
 
 # Install git and dependency for gitk
-sudo pacman -S git tk --noconfirm
+sudo pacman -S git tk --noconfirm --needed
 
 # Standard folder
     mkdir -p "$HOME"/Pictures "$HOME"/Documents "$HOME"/Desktop "$HOME"/Downloads
 
     sudo pacman -Syu
 
-    sudo pacman -S lua --noconfirm
-    sudo pacman -S xorg-xmodmap --noconfirm
-    
+    sudo pacman -S lua --noconfirm --needed
+    sudo pacman -S xorg-xmodmap --noconfirm --needed
+    yay -S xcape --noconfirm --needed
+
 # Install yay
-    cd /tmp
-    if [[ ! -d /tmp/yay  ]]; then
-        git clone https://aur.archlinux.org/yay.git
-        cd yay
-        makepkg -si
-    fi
+    command -v yay ||
+        $(cd /tmp
+          git clone https://aur.archlinux.org/yay.git
+          cd yay
+          makepkg -si
+        )
 
 # utils
     # file compression
-    sudo pacman -S zip unzip --noconfirm
+    sudo pacman -S zip unzip --noconfirm --needed
     # bluetooth
-    sudo pacman -S bluez bluez-utils --noconfirm
+    sudo pacman -S bluez bluez-utils --noconfirm --needed
     sudo systemctl enable bluetooth.service
 
-    sudo pacman -S xclip --noconfirm
-    sudo pacman -S ctags --noconfirm
+    sudo pacman -S xclip --noconfirm --needed
+    sudo pacman -S ctags --noconfirm --needed
 
-    sudo pacman -S network-manager-applet --noconfirm
+    sudo pacman -S network-manager-applet --noconfirm --needed
 
-    sudo pacman -S light --noconfirm
+    sudo pacman -S light --noconfirm --needed
     # file converter
-    sudo pacman -S texlive-most --noconfirm
+    sudo pacman -S texlive-most --noconfirm --needed
 
     # use for managing window nodes
-    sudo pacman -S xdo --noconfirm
-    sudo pacman -S xdotool --noconfirm
+    sudo pacman -S xdo --noconfirm --needed
+    sudo pacman -S xdotool --noconfirm --needed
 
-    sudo pacman -S exa --noconfirm # better ls
-    sudo pacman -S fd --noconfirm # better find
+    sudo pacman -S exa --noconfirm --needed # better ls
+    sudo pacman -S fd --noconfirm --needed # better find
 
-    sudo pacman -S jq --noconfirm
+    sudo pacman -S jq --noconfirm --needed
 
     # for debugging
-    sudo pacman -S peek --noconfirm
-    yay -S screenkey --noconfirm
+    sudo pacman -S peek --noconfirm --needed
+    yay -S screenkey --noconfirm --needed
 
 # networking
-    sudo pacman -S netcat --noconfirm
-    sudo pacman -S wget --noconfirm
-    sudo pacman -S nmap --noconfirm
-    sudo pacman -S lsof --noconfirm
+    sudo pacman -S netcat --noconfirm --needed
+    sudo pacman -S wget --noconfirm --needed
+    sudo pacman -S nmap --noconfirm --needed
+    sudo pacman -S lsof --noconfirm --needed
 
     # dig
-    sudo pacman -S bind-tools --noconfirm
+    sudo pacman -S bind-tools --noconfirm --needed
 
 # terminal and multiplexer
-    sudo pacman -S tmux konsole --noconfirm
+    sudo pacman -S tmux konsole --noconfirm --needed
 
 # Searching tool
-    sudo pacman -S ripgrep --noconfirm
-    sudo pacman -S the_silver_searcher --noconfirm
+    sudo pacman -S ripgrep --noconfirm --needed
+    sudo pacman -S the_silver_searcher --noconfirm --needed
 
 # File manager
-    sudo pacman -S nautilus ranger dolphin --noconfirm
+    sudo pacman -S nautilus ranger dolphin --noconfirm --needed
     # yay -S nautilus-dropbox
-    yay -S dropbox --noconfirm
+    yay -S dropbox --noconfirm --needed
 
 # Install zsh
-    sudo pacman -S zsh --noconfirm
-    yay -S stderred-git --noconfirm
+    sudo pacman -S zsh --noconfirm --needed
+    yay -S stderred-git --noconfirm --needed
 
 # Desktop applications
     # kde asthetics
-    sudo pacman -S latte-dock --noconfirm
-    sudo pacman -S plasma5-applets-active-window-control --noconfirm
-    yay -S sierrabreeze-kwin-decoration-git --noconfirm
-    sudo pacman -S kvantum-q --noconfirm
-    yay -S mcmojave-kde-theme-git --noconfirm
+    sudo pacman -S latte-dock --noconfirm --needed
+    sudo pacman -S plasma5-applets-active-window-control --noconfirm --needed
+    yay -S sierrabreeze-kwin-decoration-git --noconfirm --needed
+    sudo pacman -S kvantum-qt5 --noconfirm --needed
+    yay -S mcmojave-kde-theme-git --noconfirm --needed
 
 # Volume manager
-    sudo pacman -S pavucontrol pulseaudio pulseaudio-alsa --noconfirm
+    sudo pacman -S pavucontrol pulseaudio pulseaudio-alsa --noconfirm --needed
     # required to play sound over bluetooth
-    sudo pacman -S pulseaudio-bluetooth --noconfirm
+    sudo pacman -S pulseaudio-bluetooth --noconfirm --needed
 
-# Show cpu temperatures (lm-sensors)
-    sudo pacman -S i2c-tools lm-sensors --noconfirm
+# Show cpu temperatures (lm_sensors)
+    sudo pacman -S i2c-tools lm_sensors --noconfirm --needed
 
 # Dependencies for screenshot
-    sudo pacman -S flameshot --noconfirm
+    sudo pacman -S flameshot --noconfirm --needed
 
 # Internet Browser
-    sudo pacman -S firefox-developer-edition --noconfirm
+    sudo pacman -S firefox-developer-edition --noconfirm --needed
 
 # Display Compositor
-    sudo pacman -S picom --noconfirm
+    sudo pacman -S picom --noconfirm --needed
 
 # Window Manager
-    sudo pacman -S plasma --noconfirm
+    sudo pacman -S plasma --noconfirm --needed
     # used to check the keycode using `xev`
-    sudo pacman -S xorg-xev --noconfirm
-    yay -S polybar --noconfirm
+    sudo pacman -S xorg-xev --noconfirm --needed
+    yay -S polybar --noconfirm --needed
 # gtk themes
     # create filepath for configurations
     mkdir -p ~/.config/gtk-1.0
     mkdir -p ~/.config/gtk-2.0
 
-    sudo pacman -S lxappearance --noconfirm
+    sudo pacman -S lxappearance --noconfirm --needed
 
 # development
     # install docker
-        sudo pacman -S docker docker-compose --noconfirm
+        sudo pacman -S docker docker-compose --noconfirm --needed
         sudo systemctl start docker
         sudo systemctl enable docker
         sudo usermod -aG docker "$USER"
 
     # python
-        sudo pacman -S pyenv --noconfirm
-        sudo pacman -S python-pytest --noconfirm
-        sudo pacman -S python-pip --noconfirm
-        sudo pacman -S python-pipenv --noconfirm
-        sudo pacman -S python-virtualenv --noconfirm
-        sudo pacman -S python-virtualenvwrapper --noconfirm
+        sudo pacman -S pyenv --noconfirm --needed
+        sudo pacman -S python-pytest --noconfirm --needed
+        sudo pacman -S python-pip --noconfirm --needed
+        sudo pacman -S python-pipenv --noconfirm --needed
+        sudo pacman -S python-virtualenv --noconfirm --needed
+        sudo pacman -S python-virtualenvwrapper --noconfirm --needed
 
         # for vim-coc integration
-        sudo pacman -S python-pynvim python-jedi --noconfirm
+        sudo pacman -S python-pynvim python-jedi --noconfirm --needed
 
     # nodejs
-        yay -S nvm --noconfirm
+        yay -S nvm --noconfirm --needed
 
         NVM_SOURCE=/usr/share/nvm
         [ -s "$NVM_SOURCE"/nvm.sh ] && . "$NVM_SOURCE"/nvm.sh  # Load NVM
@@ -153,59 +154,59 @@ sudo pacman -S git tk --noconfirm
         # nvm alias default <version>
 
     # ??
-        sudo pacman -S perl-json --noconfirm
+        sudo pacman -S perl-json --noconfirm --needed
 
     # Text editor
         # neovim
-        sudo pacman -S neovim python-pynvim --noconfirm
+        sudo pacman -S neovim python-pynvim --noconfirm --needed
         sudo npm install -g neovim
 
         # used for previewing
-        sudo pacman -S bat --noconfirm
+        sudo pacman -S bat --noconfirm --needed
 
         # emacs
-        yay -S emacs27-git --noconfirm
+        yay -S emacs27-git --noconfirm --needed
         git clone --depth 1 https://github.com/hlissner/doom-emacs "$XDG_CONFIG_HOME"/emacs
         "$XDG_CONFIG_HOME"/emacs/bin/doom install 
         ln -s "$XDG_CONFIG_HOME"/emacs/bin/doom ~/.local/bin/doom
 
-        yay -S visual-studio-code-bin --noconfirm
+        yay -S visual-studio-code-bin --noconfirm --needed
 
         # Dependency
-        yay -S python-epc python-importmagic --noconfirm
+        yay -S python-epc python-importmagic --noconfirm --needed
 
         # Spell checker for emacs
-        sudo pacman -S aspell aspell-en --noconfirm
+        sudo pacman -S aspell aspell-en --noconfirm --needed
 
         # lsp
-        sudo pacman -S python-language-server --noconfirm
-        yay -S typescript-language-server-bin dockerfile-language-server-bin bash-language-server --noconfirm
+        sudo pacman -S python-language-server --noconfirm --needed
+        yay -S typescript-language-server-bin dockerfile-language-server-bin bash-language-server --noconfirm --needed
 
 
         # linters
 
             # python
-            sudo pacman -S python-black mypy python-pylint python-isort autopep8 --noconfirm
+            sudo pacman -S python-black mypy python-pylint python-isort autopep8 --noconfirm --needed
             # auto remove unused imports
-            yay -S python-autoflake --noconfirm
+            yay -S python-autoflake --noconfirm --needed
 
             # bash
-            yay -S shellcheck-static --noconfirm
+            yay -S shellcheck-static --noconfirm --needed
 
             # js
             sudo npm install -g import-js
             sudo npm install -g eslint-config-airbnb
             sudo npm install -g install-peerdeps
-            yay -S babel-eslint --noconfirm
-            sudo pacman -S eslint prettier --noconfirm
-            sudo pacman -S tidy --noconfirm
+            yay -S babel-eslint --noconfirm --needed
+            sudo pacman -S eslint prettier --noconfirm --needed
+            sudo pacman -S tidy --noconfirm --needed
 
 
 # maintanence
-    sudo pacman -S cronie --noconfirm
+    sudo pacman -S cronie --noconfirm --needed
     sudo systemctl enable cronie
-    yay -S timeshift --noconfirm
-    sudo pacman -S pacman-contrib --noconfirm # to install paccache
+    yay -S timeshift --noconfirm --needed
+    sudo pacman -S pacman-contrib --noconfirm --needed # to install paccache
 
 # fonts
     # to resolve nerd-fonts-complete error
@@ -213,14 +214,14 @@ sudo pacman -S git tk --noconfirm
     sudo mkdir -p /usr/share/fonts/OTF
 
     # zsh tmux themes
-        sudo pacman -S powerline-fonts --noconfirm
+        sudo pacman -S powerline-fonts --noconfirm --needed
 
     # install random chinese fonts for internet browser
-        sudo pacman -S adobe-source-han-sans-cn-fonts --noconfirm
-        sudo pacman -S adobe-source-han-sans-tw-fonts --noconfirm
-        sudo pacman -S adobe-source-han-serif-cn-fonts --noconfirm
-        sudo pacman -S adobe-source-han-serif-tw-fonts --noconfirm
-        sudo pacman -S adobe-source-han-sans-otc-fonts --noconfirm
+        sudo pacman -S adobe-source-han-sans-cn-fonts --noconfirm --needed
+        sudo pacman -S adobe-source-han-sans-tw-fonts --noconfirm --needed
+        sudo pacman -S adobe-source-han-serif-cn-fonts --noconfirm --needed
+        sudo pacman -S adobe-source-han-serif-tw-fonts --noconfirm --needed
+        sudo pacman -S adobe-source-han-sans-otc-fonts --noconfirm --needed
 
     # p10k
         
@@ -232,48 +233,48 @@ sudo pacman -S git tk --noconfirm
         # makepkg -sci BUILDDIR=.
 
     # polybar
-        yay -S ttf-material-design-icons --noconfirm
-        yay -S ttf-font-awesome-4 --noconfirm
+        yay -S ttf-material-design-icons --noconfirm --needed
+        yay -S ttf-font-awesome-4 --noconfirm --needed
 
     # emacs fallback unicode glyph fonts
-        yay -S ttf-symbola-infinality --noconfirm
+        yay -S ttf-symbola-infinality --noconfirm --needed
 
     # Fira Code
         wget --directory-prefix ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf
 
 # keyrings
-    sudo pacman -S gnome-keyring seahorse --noconfirm
-    sudo pacman -S keychain --noconfirm
-    yay -S bitwarden-bin bitwarden-cli --noconfirm
+    sudo pacman -S gnome-keyring seahorse --noconfirm --needed
+    sudo pacman -S keychain --noconfirm --needed
+    yay -S bitwarden-bin bitwarden-cli --noconfirm --needed
 
 # mail client
-    yay -S mu --noconfirm
+    yay -S mu --noconfirm --needed
     # imap
-    sudo pacman -S isync --noconfirm
+    sudo pacman -S isync --noconfirm --needed
     mkdir -p ~/.local/share/mail/school
-    sudo pacman -S thunderbird --noconfirm
+    sudo pacman -S thunderbird --noconfirm --needed
 
 # bloat
-    sudo pacman -S neofetch --noconfirm
-    yay -S python-grip --noconfirm
+    sudo pacman -S neofetch --noconfirm --needed
+    yay -S python-grip --noconfirm --needed
 
 # photoshop
-    sudo pacman -S gimp --noconfirm
+    sudo pacman -S gimp --noconfirm --needed
 # cheat
-    yay -S cheat-git --noconfirm
+    yay -S cheat-git --noconfirm --needed
     git clone https://github.com/cheat/cheatsheets.git ~/.local/share/cheat/community
 
 # fixes keychron keyboard
     echo "options hid_apple fnmode=0" | sudo tee /etc/modprobe.d/hid_apple.conf
 
 # video client
-    sudo pacman -S mpv --noconfirm
+    sudo pacman -S mpv --noconfirm --needed
 
 # softwares
-    yay -S zoom --noconfirm
-    yay -S team --noconfirm
+    yay -S zoom --noconfirm --needed
+    yay -S team --noconfirm --needed
 
 # binaries
-    yay -S pciutils --noconfirm # lspci
+    yay -S pciutils --noconfirm --needed # lspci
 
 reboot
